d9c16232c918f1666cf6049eae98501c95255142
BrunoBrandao17 <brunomachad17@gmail.com>
2026-08-27 18:59:54 -0300

    Adicionado permissões de diferentes usuários (#7)

--- src/main/java/com/example/Helpdesk/repository/ChamadoRepository.java
+++ src/main/java/com/example/Helpdesk/repository/ChamadoRepository.java
@@ -1,9 +1,16 @@
 package com.example.Helpdesk.repository;
 
 import com.example.Helpdesk.model.ChamadoModel;
+import com.example.Helpdesk.model.ChamadosEnum.NivelSuporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChamadoRepository extends JpaRepository<ChamadoModel, Long> {
+
+    List<ChamadoModel> findBySolicitanteId(Long usuarioId);
+    List<ChamadoModel> findByNivelAtual(NivelSuporte nivelAtual);
+    List<ChamadoModel> findByNivelAtualIn(List<NivelSuporte> niveis);
 }
